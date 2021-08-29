2c88ced0c8802e7ded1fc4549d8d9a65f98f4b77
fadhil rahman <fadhilrahman920@gmail.com>
2021-08-30 00:32:40 +0700

    memperbarui db

--- maya-laundry.sql
+++ maya-laundry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 27 Agu 2021 pada 17.20
+-- Waktu pembuatan: 29 Agu 2021 pada 19.31
 -- Versi server: 10.4.20-MariaDB
 -- Versi PHP: 7.4.22
 
